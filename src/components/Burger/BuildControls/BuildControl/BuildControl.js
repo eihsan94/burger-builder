04f75da6fbf1b04b761c758baefbaa46c03e357d
ウンク ムハンマド イサン <bw0021@birdworks.local>
2020-02-04 09:14:42 +0900

    added NEUMORPHISM styles

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classes from './BuildControl.module.css'
+import { FaPlus, FaMinus } from 'react-icons/fa';
 
 function BuildControl(props) {
     return (
         <div className={classes.BuildControl}>
             <div className={classes.Label}>{props.label}</div>
-            <button className={classes.Less}>Less</button>
-            <button className={classes.More}>More</button>
+            <button className={classes.Less}><FaMinus /></button>
+            <button className={classes.More}><FaPlus /></button>
         </div>
     )
 }
